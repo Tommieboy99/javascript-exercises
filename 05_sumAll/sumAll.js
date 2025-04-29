6e122248b06e2068eb13086b7fa43f95d16a3162
Tommieboy99 <tomkoek99@hotmail.com>
2025-04-29 18:09:45 +0200

    made a function with an input of 2 numbers, counts the sum of those numbers and all the numbers inside of their range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if (numberOne <= 0 || numberTwo <= 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) {
+        return "ERROR";
+    }
 
+    let totalSum = 0;
+
+    let start = Math.min(numberOne, numberTwo);
+    let end = Math.max(numberOne, numberTwo)
+
+    for (i = start; i <= end; i++ ) {
+        totalSum += i;
+    }
+
+    return totalSum
 };
 
 // Do not edit below this line
