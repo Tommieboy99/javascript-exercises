bc23a2bb2d5a682c271366dbd5483237ee0953af
Tommieboy99 <tomkoek99@hotmail.com>
2025-04-29 14:46:11 +0200

    made a functon to reversewords!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordReversed = word.split('');
+    wordReversed.reverse();
+    let reversed = wordReversed.join('');
 
+    return reversed
 };
 
 // Do not edit below this line
