47288ba795254b2d000151d19a913cc4ebd31bc7
Tommieboy99 <tomkoek99@hotmail.com>
2025-04-29 19:37:17 +0200

    made a function that converts celsius<>fahrenheit and returns a temperature with 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+  if (inputFahrenheit === 32) {
+    return 0;
+  }
+  let Celcius = (inputFahrenheit - 32) * 5 / 9;
+  return Number(Celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelcius) {
+  if (inputCelcius === 0) {
+    return 32;
+  }
+  let Fahrenheit = (inputCelcius * 9/5) + 32;
+  return Number(Fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
