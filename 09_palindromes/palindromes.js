4c3c86f1555a51fe8e07de57e6bad20093f78533
Tommieboy99 <tomkoek99@hotmail.com>
2025-05-08 09:51:47 +0200

    Made the function work to return true if a word is a palindrome and false if not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (...words) {
+    return words.every(word => {
+        const cleanedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+        const wordReversed = cleanedWord.split('').reverse().join('');
+        return wordReversed === cleanedWord;
+    });
 };
 
 // Do not edit below this line
