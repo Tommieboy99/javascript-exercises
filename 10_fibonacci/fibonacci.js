0910dc911a23082450c9a478965123870bc68d0a
Tommieboy99 <tomkoek99@hotmail.com>
2025-05-08 09:52:39 +0200

    Made the function work to return the fibonacci sum

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
 
+    if (isNaN(number) || number < 0) return "OOPS";
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= number; i++) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
